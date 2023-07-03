62fec76dbf117d9dfe2ebaf11607da1f0bfe812c
Votybe <66923802+Votybe@users.noreply.github.com>
2023-07-03 21:01:29 +0200

    delete usless comment

--- src/views/movie-details.vue
+++ src/views/movie-details.vue
@@ -30,12 +30,12 @@ onMounted(() => {
       alt="hello"
       class="movie-image"
   />
- 
+
 </template>
 
 <style lang="scss" scoped>
 img {
-  width: 300px; /* Adjust this to your desired width */
+  width: 300px;
   height: 450px; /* Adjust this to your desired height */
   object-fit: cover;
 }
